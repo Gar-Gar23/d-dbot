748867a8ed4cb3b644538d528eaf37a39de024ea
Gar-Gar23 <g.dambra233@gmail.com>
2021-02-16 21:42:37 -0600

    arg number 3 -6 , 'd8 - d20'

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,10 @@
 exports.run = async (client, message, args, level) => { 
   if (args[0] == "d4") { var value = getRandomIntInclusive(1,4) }
   if (args[0] == "d6") {var value = getRandomIntInclusive(1,6) }
+  if (args[0] == "d8") {var value = getRandomIntInclusive(1,8) }
+  if (args[0] == "d10") {var value = getRandomIntInclusive(1,10) }
+  if (args[0] == "d12") {var value = getRandomIntInclusive(1,12) }
+  if (args[0] == "d20") {var value = getRandomIntInclusive(1,20) }
   function getRandomIntInclusive(min, max) {
     
     min = Math.ceil(min);
