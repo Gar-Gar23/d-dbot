2974735d436be34e34a333c5132d420b57cfa6f4
Gar-Gar23 <g.dambra233@gmail.com>
2021-02-16 19:03:59 -0600

    adjustment of arg and msg

--- commands/roll.js
+++ commands/roll.js
@@ -1,15 +1,13 @@
 exports.run = async (client, message, args, level) => { 
   function getRandomIntInclusive(min, max) {
-    min = Math.ceil(0);
+    if (args[0] == "d4") { var value = getRandomIntInclusive() }
+    min = Math.ceil(1);
     max = Math.floor(4);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    message.channel.send(
+     Math.floor(Math.random() * (max - min + 1) + min)); //The maximum is inclusive and the minimum is inclusive
+    }
   }
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(6);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
-};
+
 
 exports.conf = {
   enabled: true,
