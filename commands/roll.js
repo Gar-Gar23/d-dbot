57237748d6cbcb9621972e80e48579b708b9d60f
Gar-Gar23 <g.dambra233@gmail.com>
2021-02-16 20:27:55 -0600

    arg and message functions

--- commands/roll.js
+++ commands/roll.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args, level) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
-  message.channel.send(math.floor(random() * ('max - min + 1 min')
+  message.channel.send(value)
 };
     
 
